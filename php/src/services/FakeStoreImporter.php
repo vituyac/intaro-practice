c33b0e540fa8b20c678c1401044dce014507fdf2
ed573 <ed573808@gmail.com>
2025-07-04 16:04:56 +0300

    feat: import categories

--- php/src/services/FakeStoreImporter.php
+++ php/src/services/FakeStoreImporter.php
@@ -18,8 +18,41 @@ class FakeStoreImporter {
             $this->importCategories();
             
             //Импорт продуктов и торговых предложений
-            $this->importProducts();
+            //$this->importProducts();
       }
 
+      private function importCategories() {  
+            $json = file_get_contents($this->categoriesUrl);
+            $data = json_decode($json, true);
+            $categories = $data['categories'] ?? [];
+
+            
+            // Проверяем наличие родительской категории Электроника
+            $stmt = $this->db->prepare("SELECT id FROM section WHERE title = ?");
+            $stmt->execute(['Electronics']);
+            $parent = $stmt->fetch();
+
+            if (!$parent) {
+                  $stmt = $this->db->prepare("INSERT INTO section (title, parent_id) VALUES (?, NULL)");
+                  $stmt->execute(['Electronics']);
+                  $parentId = $this->db->lastInsertId();
+            } else {
+                  $parentId = $parent['id'];
+            }
+
+            foreach ($categories as $categoryName) {
+                  var_dump($categoryName);
+                  $stmt = $this->db->prepare("SELECT id FROM section WHERE title = ?");
+                  $stmt->execute([ucfirst($categoryName)]);
+                  $existing = $stmt->fetch();
+                  
+                  if (!$existing) {
+                  $stmt = $this->db->prepare(
+                        "INSERT INTO section (title, parent_id) VALUES (?, ?)"
+                  );
+                  $stmt->execute([ucfirst($categoryName),$parentId]);
+                  }
+            }
+      }      
 }
 
