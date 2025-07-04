8d6e480b4253ec0e587ba1b630fca73a787679d1
Nikita <20_nik_05@mail.ru>
2025-07-04 17:31:44 +0300

    chore(index): зарегистрированы эндпоинты для получения способов доставки и оплаты

--- php/public/index.php
+++ php/public/index.php
@@ -2,8 +2,13 @@
     require __DIR__ . '/../vendor/autoload.php';
 
     use App\core\Router;
+    use App\controllers\RetailCrmController;
 
     $router = new Router();
+    $retailCrm = new RetailCrmController();
+
+    $router->get('/delivery-types', [$retailCrm, 'deliveryTypes']);
+    $router->get('/payment-types', [$retailCrm, 'paymentTypes']);
 
     $router->resolve();
 ?>
