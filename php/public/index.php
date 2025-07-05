5de38316731d90c63de40283d8b86cd114f4378a
Cblp123 <kukikristmas@gmail.com>
2025-07-05 16:52:01 +0300

    feat: добавлены новые router для каталога

--- php/public/index.php
+++ php/public/index.php
@@ -4,15 +4,22 @@
     use App\core\Router;
     use App\controllers\RetailCrmController;
     use App\controllers\UserController;
+    use App\controllers\SectionController;
+    use App\controllers\OfferController;
 
     $router = new Router();
     $retailCrm = new RetailCrmController();
     $users = new UserController();
+    $sections = new SectionController();
+    $offers = new OfferController();
 
     $router->get('/delivery-types', [$retailCrm, 'deliveryTypes']);
     $router->get('/payment-types', [$retailCrm, 'paymentTypes']);
 
     $router->post('/register', [$users, 'register']);
+
+    $router->get('/section', [$sections, 'showSection']);
+    $router->get('/offer', [$offers, 'showOffer']);
 
     $router->resolve();
 ?>
