09720b3bf982b0a23fc0f83be8071a0a0179b815
Nikita <20_nik_05@mail.ru>
2025-07-05 13:59:45 +0300

    chore(index): зарегистрирован эндпоинт для оформления заказа

--- php/public/index.php
+++ php/public/index.php
@@ -3,12 +3,15 @@
 
     use App\core\Router;
     use App\controllers\RetailCrmController;
+    use App\controllers\OrderController;
 
     $router = new Router();
     $retailCrm = new RetailCrmController();
+    $order = new OrderController();
 
     $router->get('/delivery-types', [$retailCrm, 'deliveryTypes']);
     $router->get('/payment-types', [$retailCrm, 'paymentTypes']);
+    $router->post('/basket/making-an-order', [$order, 'pushOrderCrm']);
 
     $router->resolve();
 ?>
