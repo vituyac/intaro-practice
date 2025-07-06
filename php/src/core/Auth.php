9fce311474e5f0906786016cb7a09515dd5f6744
vituyac <skisterev78@mail.ru>
2025-07-06 14:52:12 +0300

    refactor(Auth): code error

--- php/src/core/Auth.php
+++ php/src/core/Auth.php
@@ -4,8 +4,8 @@ namespace App\core;
 class Auth {
     public static function isAuthenticated() {
         if (!isset($_SESSION['user_id'])) {
-            http_response_code(403);
-            exit(json_encode(['error' => 'Недостаточно прав']));
+            http_response_code(401);
+            exit(json_encode(['error' => 'Требуется аутентификация']));
         }
         return true;
     }
